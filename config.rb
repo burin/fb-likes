cbdfa668fd16aa8b0f78f92649362344248d0d30
Burin Asavesna <burin@happiness-2.(none)>
2010-10-13 23:45:51 -0500

    moving files around

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "_css"
-sass_dir = "src"
-images_dir = "_img"
-javascripts_dir = "_js"
+css_dir = "public/_css"
+sass_dir = "stylesheets"
+images_dir = "public/_img"
+javascripts_dir = "public/_js"
 output_style = :compressed
 line_comments = false
 # To enable relative paths to assets via compass helper functions. Uncomment:
